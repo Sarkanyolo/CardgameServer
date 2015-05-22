5e14aa13649babe55528b598d39f208001c2cb11
Sarkanyolo <gyuri.deak@gmail.com>
2015-05-22 13:30:43 +0200

    Handle duel answers
    Auto clean inactive players

--- s.php
+++ s.php
@@ -1,11 +1,58 @@
 <?php
 require_once('db.php');
 
+function myexit($str){
+    db::disconnect();
+    exit($str);
+}
+
+function getnick($id){
+    $v = db::quick_fetch("select nick from users where id='$id'");
+    if(empty($v)) return NULL;
+    return $v['nick'];
+}
+
+function getid($nick){
+    $v = db::quick_fetch("select id from users where nick='$nick'");
+    if(empty($v)) return NULL;
+    return $v['id'];
+}
+
+function cleanold(){
+    db::send('delete from users where lastaction < ('. time() .'-180)');
+    db::send('delete from messages where userid not in (select id from users)');
+}
+
+if(isset($_GET['gstatus'])) gstatus($_GET['gstatus']);
 if(isset($_GET['glist'])) glist($_GET['glist']);
+if(isset($_GET['aduel'])) aduel($_GET['aduel'], $_GET['me'], $_GET['op']);
 if(isset($_GET['sname'])) sname($_GET['sname']);
 if(isset($_GET['sduel'])) sduel($_GET['sduel'], $_GET['op']);
 if(isset($_GET['dname'])) dname($_GET['dname']);
 if(isset($_GET['dduel'])) dduel($_GET['dduel']);
+
+function gstatus($id){
+    $nick = getnick($id);
+    if(empty($nick)){myexit('NOK');}
+    db::send('update users set lastaction='.time()." where id='$id'");
+    $str = '';
+    
+    // Check if Duel
+    $res = db::send("select nick from users where duel='$nick'");
+    while($v = db::fetch($res)){
+        $str .= '#'.$v['nick'];
+    }
+    if($str !== ''){myexit('Duel'.$str);}
+    
+    // Check if messages
+    $res = db::send("select * from messages where userid='$id'");
+    while($v = db::fetch($res)){
+        db::send('delete from messages where id='. $v['id']);
+        myexit($v['message']);
+    }
+            
+    myexit('OK');
+}
 
 function glist($param){
     $s = '';
@@ -20,7 +67,24 @@ function glist($param){
     myexit(substr($s,1));
 }
 
-function sduel($me, $op){
+function aduel($answer, $id, $op){
+    if($answer==='NO'){
+        db::send("update users set duel='-' where nick='$op'");
+        db::send("insert into messages (userid, message) select id, 'DuelNOK' from users where nick='$op'");
+        myexit('OK');
+    } elseif($answer==='YES') {
+        $v = db::quick_fetch("select duel from users where nick='$op'");
+        if(empty($v)) myexit('NOK');
+        $nick = getnick($id);
+        db::send("insert into messages (userid, message) select id, 'Start#$nick' from users where nick='$op'");
+        db::send("insert into messages (userid, message) VALUES('$id', 'Start#$op')");
+        myexit('OK');
+    }
+    myexit('NOK');
+}
+
+function sduel($id, $op){
+    cleanold();
     $v = db::quick_fetch("select count(*) as pcs from users where nick='$op'");
     if($v['pcs']==='0'){
         myexit('DISC');
@@ -29,32 +93,33 @@ function sduel($me, $op){
     if($v['duel']!=='-'){
         myexit('DUEL');
     }
-    db::send("update users set duel='$op' where nick='$me'");
+    db::send("update users set duel='$op' where id='$id'");
     myexit('OK');
 }
 
 function sname($name){
+    cleanold();
     $v = db::quick_fetch("select count(*) as pcs from users where nick='$name'");
     if ($v['pcs'] !== '0') {
         myexit('NOK');
     }
-    db::send("insert into users (nick) VALUES ('$name')");
+    $str = substr("abcdefghijklmnopqrstuvwxyz",mt_rand(0,25),1).substr(md5(time()),1);
+    db::send("insert into users (id, nick, lastaction) VALUES ('$str', '$name', ".time().")");
+    myexit($str);
+}
+
+function dname($id){
+    cleanold();
+    db::send("delete from users where id='$id'");
+    db:send("delete from messages where userid='$id'");
+    $nick = getnick($id);
+    db::send("update users set duel='-' where duel='$nick'");
     myexit('OK');
 }
 
-function dname($name){
-    db::send("delete from users where nick='$name'");
+function dduel($id){
+    db::send("update users set duel='-' where id='$id'");
     myexit('OK');
-}
-
-function dduel($name){
-    db::send("update users set duel='-' where nick='$name'");
-    myexit('OK');
-}
-
-function myexit($str){
-    db::disconnect();
-    exit($str);
 }
 
 db::disconnect();
@@ -79,6 +144,18 @@ db::disconnect();
             <table>
                 <tr>
                     <td>
+                        aduel=<span class='respOK'>YES</span>&me=<i>myid</i>&op=<i>opponentname</i>
+                        <br>
+                        aduel=<span class='respOK'>NO</span>&me=<i>myid</i>&op=<i>opponentname</i>
+                    </td>
+                    <td>
+                        Accept/Refuse the duel<br><br>
+                        <span class='respOK'>OK</span> - if everything OK<br>
+                        <span class='respNOK'>NOK</span> - if challenger cancelled / disconnected
+                    </td>
+                </tr>
+                <tr>
+                    <td>
                         glist=<i>param</i></a>
                         <br><br>Possible params:<br>
                         <i>all</i> - List everyone<br>
@@ -88,18 +165,19 @@ db::disconnect();
                     <td><span class='respOK'>user1#user2#user3#user4</span></td>
                 </tr>
                 <tr>
-                    <td>gstatus=<i>myname</i></td>
+                    <td>gstatus=<i>myid</i></td>
                     <td>See table below...</td>
                 </tr>
                 <tr>
                     <td>sname=<i>username</i></td>
                     <td>
-                        Sets the <i>username</i> (English, lowercase)<br><br>
-                        Returns <span class='respNOK'>NOK</span> if reserved.
+                        Log in <i>username</i> (English, lowercase)<br><br>
+                        Returns <span class='respNOK'>NOK</span> if reserved.<br>
+                        Returns a string (length:32), with <span class='respOK'>unique id</span> if OK
                     </td>
                 </tr>
                 <tr>
-                    <td>sduel=<i>myname</i>&op=<i>username</i></td>
+                    <td>sduel=<i>myid</i>&op=<i>username</i></td>
                     <td>
                         Ask for opponent, if he wants to duel<br><br>
                         <span class='respOK'>OK</span> - Invite sent<br>
@@ -109,18 +187,23 @@ db::disconnect();
                     </td>
                 </tr>
                 <tr>
-                    <td>dname=<i>username</i></td>
-                    <td>Deletes the <i>username</i><br>Returns <span class='respOK'>OK</span></td>
+                    <td>dname=<i>myid</i></td>
+                    <td>Logout<br>Returns <span class='respOK'>OK</span></td>
                 </tr>
                 <tr>
-                    <td>dduel=<i>myname</i></td>
-                    <td>Not waiting for the opponent any more<br>Returns <span class='respOK'>OK</span></td></tr>
+                    <td>dduel=<i>myid</i></td>
+                    <td>Not waiting for the opponent any more<br>Returns <span class='respOK'>OK</span></td>
+                </tr>
             </table><br>
             <h4>Possible responses of the <i>gstatus</i> command:</h4>
             <table>
                 <tr>
                     <td>When there is no new info to the player</td>
                     <td><span class='respOK'>OK</span></td>
+                </tr>
+                <tr>
+                    <td>When id not found; relogin is needed</td>
+                    <td><span class='respNOK'>NOK</span></td>
                 </tr>
                 <tr>
                     <td>When waiting for duel response</td>
@@ -130,7 +213,12 @@ db::disconnect();
                     <td>When somebody(ies) asked you for duel</td>
                     <td><span class='respOK'>Duel#user1#user2#user3</span></td>
                 </tr>
-            </table>
+                <tr>
+                    <td>When the game starts</td>
+                    <td><span class='respOK'>Start#user1</span></td>
+                </tr>
+            </table><br>
+            PHP time: <?=time() ?>
         </center>
 </body>
 </html>
